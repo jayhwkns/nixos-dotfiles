e4eeff5b9c5d610395439b7851d1a31de5a5308c
jayhwkns <hawkinsjr27@gmail.com>
2026-03-30 19:27:35 -0400

    removed unused options in configuration

--- configuration.nix
+++ configuration.nix
@@ -2,8 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
-
+{ pkgs, ... }:
 let
   # Creates a package that symlinks the plasma menu to the standard location
   kde-application-menu-fix = pkgs.runCommandLocal "xdg-application-menu" { } ''
