aca18206b20a2ca77c7a65e78312fc79fe4158cc
jayhwkns <hawkinsjr27@gmail.com>
2026-03-31 11:50:58 -0400

    laptop battery

--- laptop.nix
+++ laptop.nix
@@ -1,6 +1,12 @@
-{...}:
+{ pkgs, ... }:
 {
   # firmware updating
   services.fwupd.enable = true;
   services.power-profiles-daemon.enable = true;
+  powerManagement.enable = true;
+  services.upower.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    acpi
+  ];
 }
