ef46641315839bdcd3d30efb949d8a964989f454
jayhwkns <hawkinsjr27@gmail.com>
2026-04-01 19:30:37 -0400

    fingerprint

--- laptop.nix
+++ laptop.nix
@@ -2,9 +2,14 @@
 {
   # firmware updating
   services.fwupd.enable = true;
+
+  # battery
   services.power-profiles-daemon.enable = true;
   powerManagement.enable = true;
   services.upower.enable = true;
+
+  # fingerprint
+  services.fprintd.enable = true;
 
   environment.systemPackages = with pkgs; [
     acpi
