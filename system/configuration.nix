161bc66769708a4bb2aaeeff24ff1072fb0696b8
jayhwkns <hawkinsjr27@gmail.com>
2026-08-05 14:47:21 -0400

    Install git lfs

--- system/configuration.nix
+++ system/configuration.nix
@@ -158,13 +158,17 @@
     };
   };
 
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     helix
     vim
     wget
-    git
     alacritty
     openssl
     gnumake
